67e120af8e155d8265b59d06a4a1682c9a0b38dc
carlosvillarvega <66485662+carlosvillarvega@users.noreply.github.com>
2020-06-09 09:50:16 +0200

    Create image.statistics.java

---
+++ image.statistics.java
@@ -0,0 +1,10 @@
+package com.example.spaceX.model;
+
+
+public class ImageStatistics {
+	
+	private static final long serialVersionUID = 1L;
+	private String queryText;
+	private int totalTweets;
+	private int tweetsWithImages;
+	
