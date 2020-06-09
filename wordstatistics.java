997775026c0002ad9169a369c0433ff0ecf1763b
carlosvillarvega <66485662+carlosvillarvega@users.noreply.github.com>
2020-06-09 15:34:09 +0200

    Update wordstatistics.java

--- wordstatistics.java
+++ wordstatistics.java
@@ -4,4 +4,27 @@ public class WordStatistics {
 	
 	public String word;
 	public int appearances;
-  
+  public WordStatistics(String word, int appearances) {
+		super();
+		this.word = word;
+		this.appearances = appearances;
+	}
+	
+	
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public int getAppearances() {
+		return appearances;
+	}
+	public void setAppearances(int appearances) {
+		this.appearances = appearances;
+	}
+	
+	
+
+}
+
